616a023a73653f844b6e87b536a722567af1fc24
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2023-11-03 23:08:59 +0100

    Добавил ф-цию print() для вывода результата

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis","blanchefort/rubert-base-cased-sentiment")
-classifier("Я ненавижу инженерию машинного обучения!")
+print(classifier("Я ненавижу инженерию машинного обучения!"))
 
